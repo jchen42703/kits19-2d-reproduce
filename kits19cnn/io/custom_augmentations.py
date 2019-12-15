eb953d5fe10763b7ab3222c25e1b9ea329d67c15
jchen42703 <jchen42703@gmail.com>
2019-12-15 18:10:26 -0500

    Converted resize_data_and_seg to work without the batch size dimension because it's unnecessary

--- kits19cnn/io/custom_augmentations.py
+++ kits19cnn/io/custom_augmentations.py
@@ -6,8 +6,8 @@ def resize_data_and_seg(data, size, seg=None, order_data=3,
                         order_seg=1, cval_seg=0):
     """
     Args:
-        data (np.ndarray): shape (b, c, h, w (, d))
-        seg (np.ndarray): shape (b, c, h, w (, d)). Defaults to None.
+        data (np.ndarray): shape (c, h, w (, d))
+        seg (np.ndarray): shape (c, h, w (, d)). Defaults to None.
         size (list/tuple of int): size to resize to
             does not include the batch size or number of channels
         order_data (int): interpolation order for data
@@ -15,16 +15,15 @@ def resize_data_and_seg(data, size, seg=None, order_data=3,
         order_seg (int): interpolation order for seg
             (see skimage.transform.resize)
     """
-    target_data = np.ones(list(data.shape[:2]) + size)
+    target_data = np.ones([data.shape[0]] + list(size))
     if seg is not None:
-        target_seg = np.ones(list(seg.shape[:2]) + size)
+        target_seg = np.ones([seg.shape[0]] + list(size))
     else:
         target_seg = None
 
-    for b in range(len(data)):
-        target_data[b] = resize_multichannel_image(data[b], size, order_data)
-        if seg is not None:
-            for c in range(seg.shape[1]):
-                target_seg[b, c] = resize_segmentation(seg[b, c], size,
-                                                       order_seg, cval_seg)
+    target_data = resize_multichannel_image(data, size, order_data)
+    if seg is not None:
+        for c in range(seg.shape[0]):
+            target_seg[c] = resize_segmentation(seg[c], size,
+                                                order_seg, cval_seg)
     return target_data, target_seg
