a9d3a3ffcaa65cd283156e1461143b4b0dbc21ff
Benson <36316652+Jinb2@users.noreply.github.com>
2019-12-14 20:34:08 -0500

    stage1 architecture

---
+++ ResUnet.py
@@ -0,0 +1,79 @@
+import torch.nn.functional as F
+
+from utils import *
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(256,16)
+        self.down2 = Down(128,32)
+        self.down3 = Down(64,64)
+        self.down4 = Down(32,128)
+        self.down5 = Down(16,256)
+        self.up1 = Up(32, 128, bilinear)
+        self.up2 = Up(64, 64, bilinear)
+        self.up3 = Up(128, 128, bilinear)
+        self.up4 = Up(256, 256, bilinear)
+        self.outc = OutConv(256, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+
+# 3x3 convolution
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                     stride=stride, padding=1, bias=False)
+# 1x1 convolution
+def conv1x1(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=1,
+                     stride=stride, padding=1, bias=False)
+
+# Residual block
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv1x1(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv1x1(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+    def skipconnection(self, in_channels, out_channels, stride=1):
+        shortcut = conv1x1(in_channels,out_channels,stride)
