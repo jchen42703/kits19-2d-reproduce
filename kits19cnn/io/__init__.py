d22d1ec75aa2587ba5208df2d050e478eb06fb2a
jchen42703 <jchen42703@gmail.com>
2019-12-14 23:27:54 -0500

    Updated the __init__.py file

--- kits19cnn/io/__init__.py
+++ kits19cnn/io/__init__.py
@@ -1,7 +1,3 @@
-from .dataset import ClfSegVoxelDataset, VoxelDataset, TestVoxelDataset
-from .dataset_2d import SliceDataset, PseudoSliceDataset
+from .dataset import SliceDataset, PseudoSliceDataset
 from .preprocess import Preprocessor
 from .resample import resample_patient
-from .custom_transforms import ROICropTransform, RepeatChannelsTransform, \
-                               MultiClassToBinaryTransform, \
-                               RandomResizedCropTransform
