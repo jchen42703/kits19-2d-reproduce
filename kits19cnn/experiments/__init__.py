ad1edb78f5c249ba27c1cfda3f14cb7654b5ff4c
jchen42703 <jchen42703@gmail.com>
2019-12-23 13:35:50 -0500

    Fixed another careless error

--- kits19cnn/experiments/__init__.py
+++ kits19cnn/experiments/__init__.py
@@ -2,5 +2,5 @@ from .utils import get_training_augmentation, get_validation_augmentation, \
                    get_preprocessing, seed_everything
 from .train import TrainExperiment
 from .train_2d import TrainExperiment2D, TrainSegExperiment2D
-from .infer import SegmentationInferenceExperiment
+from .infer import BaseInferenceExperiment
 from .infer_2d import SegmentationInferenceExperiment2D
