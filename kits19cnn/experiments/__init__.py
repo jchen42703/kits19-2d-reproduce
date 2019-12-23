3a2eb8abb97285e3a6edc3c9aa57e67aa3def688
jchen42703 <jchen42703@gmail.com>
2019-12-23 13:17:32 -0500

    Fixed typo

--- kits19cnn/experiments/__init__.py
+++ kits19cnn/experiments/__init__.py
@@ -1,6 +1,6 @@
 from .utils import get_training_augmentation, get_validation_augmentation, \
                    get_preprocessing, seed_everything
-from .train import TrainSegExperiment
-from .train_2d import TrainSegExperiment2D
+from .train import TrainExperiment
+from .train_2d import TrainExperiment2D, TrainSegExperiment2D
 from .infer import SegmentationInferenceExperiment
 from .infer_2d import SegmentationInferenceExperiment2D
