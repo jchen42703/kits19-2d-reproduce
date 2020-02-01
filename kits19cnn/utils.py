0dbd58a6b0d8875ea3ba6a61fcace88c5c4a3ca3
Joseph <jchen42703@gmail.com>
2020-02-01 11:54:53 -0500

    maybe_to_torch and to_cuda can now handle cases where the input is None
    For neural_network.py's SegmentationNetwork's methods' "mult" argument <- defaults to None

--- kits19cnn/utils.py
+++ kits19cnn/utils.py
@@ -39,6 +39,8 @@ def sum_tensor(inp, axes, keepdim=False):
 def maybe_to_torch(d):
     if isinstance(d, list):
         d = [maybe_to_torch(i) if not isinstance(i, torch.Tensor) else i for i in d]
+    elif d is None:
+        d = None
     elif not isinstance(d, torch.Tensor):
         d = torch.from_numpy(d).float()
     return d
@@ -47,6 +49,8 @@ def maybe_to_torch(d):
 def to_cuda(data, non_blocking=True, gpu_id=0):
     if isinstance(data, list):
         data = [i.cuda(gpu_id, non_blocking=non_blocking) for i in data]
+    elif data is None:
+        data = None
     else:
         data = data.cuda(gpu_id, non_blocking=True)
     return data
