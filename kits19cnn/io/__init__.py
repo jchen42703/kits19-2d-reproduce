c6f9b2b0195d1cc830e863416636a851c3726fd0
Joseph <jchen42703@gmail.com>
2020-02-03 14:11:55 -0500

    Updated for resize_bbox

--- kits19cnn/io/__init__.py
+++ kits19cnn/io/__init__.py
@@ -4,5 +4,5 @@ from .preprocess import Preprocessor
 from .resample import resample_patient
 from .custom_transforms import CenterCrop
 from .custom_augmentations import resize_data_and_seg, crop_to_bbox, \
-                                  expand_bbox, get_bbox_from_mask
+                                  expand_bbox, get_bbox_from_mask, resize_bbox
 from .slice_sampler import SliceIDSampler
