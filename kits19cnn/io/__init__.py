f972cbeed6440ed62f9bd6caca3289a19d68022b
Joseph <jchen42703@gmail.com>
2020-02-05 23:26:49 -0500

    Added ToTensorV2

--- kits19cnn/io/__init__.py
+++ kits19cnn/io/__init__.py
@@ -2,7 +2,7 @@ from .dataset import SliceDataset, PseudoSliceDataset
 from .test_dataset import VoxelDataset, TestVoxelDataset
 from .preprocess import Preprocessor
 from .resample import resample_patient
-from .custom_transforms import CenterCrop
+from .custom_transforms import CenterCrop, ToTensorV2
 from .custom_augmentations import resize_data_and_seg, crop_to_bbox, \
                                   expand_bbox, get_bbox_from_mask, resize_bbox
 from .slice_sampler import SliceIDSampler
