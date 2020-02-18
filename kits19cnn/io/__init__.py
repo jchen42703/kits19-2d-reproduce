3647a1f2dfcd78461bb77f4d4359fea049e48d03
Joseph <jchen42703@gmail.com>
2020-02-17 20:40:30 -0500

    Added import support for SliceDatasetOnTheFly and PseudoSliceDatasetOnTheFly and alphabetically sorted the imports

--- kits19cnn/io/__init__.py
+++ kits19cnn/io/__init__.py
@@ -1,8 +1,9 @@
-from .dataset import SliceDataset, PseudoSliceDataset
-from .test_dataset import VoxelDataset, TestVoxelDataset, PredictionDataset
-from .preprocess import Preprocessor
-from .resample import resample_patient
-from .custom_transforms import CenterCrop, ToTensorV2
 from .custom_augmentations import resize_data_and_seg, crop_to_bbox, \
                                   expand_bbox, get_bbox_from_mask, resize_bbox
+from .custom_transforms import CenterCrop, ToTensorV2
+from .dataset import SliceDataset, PseudoSliceDataset
+from .dataset_onthefly import SliceDatasetOnTheFly, PseudoSliceDatasetOnTheFly
+from .preprocess import Preprocessor
+from .resample import resample_patient
 from .slice_sampler import SliceIDSampler
+from .test_dataset import VoxelDataset, TestVoxelDataset, PredictionDataset
